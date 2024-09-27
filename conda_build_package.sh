84a55fcebf9228aed2fdde05d6f6e07977c10436
Xi Zhang <13602037587@163.com>
2024-09-27 10:51:24 -0600

    Update conda_build_package.sh

--- conda_build_package.sh
+++ conda_build_package.sh
@@ -3,7 +3,6 @@ mkdir -p $PREFIX/bin
 
 cp HSD_heatmap.py $PREFIX/bin/hsdecipher
 
-cp HSD_heatmap.py $PREFIX/bin/HSD_heatmap.py
 cp HSD_add_on.py $PREFIX/bin/HSD_add_on.py
 cp HSD_batch_run.py $PREFIX/bin/HSD_batch_run.py
 cp HSD_categories.py $PREFIX/bin/HSD_categories.py
@@ -11,7 +10,6 @@ cp HSD_statistics.py $PREFIX/bin/HSD_statistics.py
 cp ko.tsv $PREFIX/bin/ko.tsv
 
 chmod a+x $PREFIX/bin/hsdecipher
-chmod a+x $PREFIX/bin/HSD_heatmap.py
 chmod a+x $PREFIX/bin/HSD_add_on.py
 chmod a+x $PREFIX/bin/HSD_batch_run.py
 chmod a+x $PREFIX/bin/HSD_categories.py
